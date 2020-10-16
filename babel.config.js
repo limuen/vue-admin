186644097a4d190abe57cbd99309558d30869200
limuen <limuen_dev@163.com>
2020-10-16 09:59:18 +0800

    新增项目去除console

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,16 @@
+const IS_PROD = process.env.NODE_ENV === 'production'
+const plugins = []
+if(IS_PROD){
+  plugins.unshift([
+    'transform-remove-console',
+    {
+      exclude: ['error', 'warn']
+    }
+  ])
+}
 module.exports = {
-  presets: [
-    '@vue/app'
-  ]
+    presets: [
+      '@vue/app'
+    ],
+    plugins
 }
